1e015b5ece5cd2d1c153e47257cd8a4831b3dbea
mojonapower <maca95@gmail.com>
2021-08-06 00:35:14 +0000

    up

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	function mandarTodos() {
+		let data = [
+			{
+				label: "tarea1",
+				done: false
+			},
+			{
+				label: "tarea2",
+				done: false
+			},
+			{
+				label: "tarea3",
+				done: false
+			},
+			{
+				label: "tarea4",
+				done: false
+			},
+			{
+				label: "tarea5",
+				done: false
+			}
+		];
+		let infoNecesaria = {
+			method: "PUT",
+			body: JSON.stringify(data),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		};
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/profemaca",
+			infoNecesaria
+		)
+			.then(res => res.json())
+			.then(respuesta => console.log(respuesta))
+			.catch(error => console.log("alog paso", error));
+	}
+	//mandarTodos();
+	function getTodos() {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/profemaca")
+			.then(res => res.json())
+			.then(respuesta => console.log(respuesta))
+			.catch(error => console.log("alog paso", error));
+	}
+	getTodos();
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>api todos</h1>
 		</div>
 	);
 };
